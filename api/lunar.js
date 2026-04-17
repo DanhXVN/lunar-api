58b7473d0655b29f146dd4d7fbb0992a4d9aeaa8
DanhXVN <danhxvn@gmail.com>
2026-04-17 15:39:15 +0700

    Update lunar.js

--- api/lunar.js
+++ api/lunar.js
@@ -5,9 +5,34 @@ export default function handler(req, res) {
   const solar = Solar.fromDate(now);
   const lunar = solar.getLunar();
 
-  return res.status(200).json({
+  // chuyển can chi sang tiếng Việt
+  const canChi = lunar.getYearInGanZhi()
+    .replace("丙", "Bính")
+    .replace("丁", "Đinh")
+    .replace("戊", "Mậu")
+    .replace("己", "Kỷ")
+    .replace("庚", "Canh")
+    .replace("辛", "Tân")
+    .replace("壬", "Nhâm")
+    .replace("癸", "Quý")
+    .replace("甲", "Giáp")
+    .replace("乙", "Ất")
+    .replace("子", "Tý")
+    .replace("丑", "Sửu")
+    .replace("寅", "Dần")
+    .replace("卯", "Mão")
+    .replace("辰", "Thìn")
+    .replace("巳", "Tỵ")
+    .replace("午", "Ngọ")
+    .replace("未", "Mùi")
+    .replace("申", "Thân")
+    .replace("酉", "Dậu")
+    .replace("戌", "Tuất")
+    .replace("亥", "Hợi");
+
+  res.status(200).json({
     duong_lich: now.toLocaleDateString("vi-VN"),
     am_lich: `${lunar.getDay()}/${lunar.getMonth()}/${lunar.getYear()}`,
-    can_chi: lunar.getYearInGanZhi()
+    can_chi: canChi
   });
 }
